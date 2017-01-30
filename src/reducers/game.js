ca2ade3add55db45b5d2b7239281f040b8c427d8
lidak <animatio.dev@gmail.com>
2017-01-30 18:07:10 +0200

    game.js description comment added

--- src/reducers/game.js
+++ src/reducers/game.js
@@ -1,13 +1,29 @@
 import { selectWinner } from '../core'
 
-let player = 'x';
+let player = 'x'
 
 let switchPlayer = () => {
-    player = player === 'x' ? 'o' : 'x';
+    player = player === 'x' ? 'o' : 'x'
 }
 
+let clearMap = () => (
+  new Array(9).fill(null)
+)
+
+/*
+
+  I don't like in general idea of containing and calculating
+  multiple state props in one reducer. But boardMap and score are
+  basically dependant on a single action (makeMove).
+  So I chose this approach because if split those two props into different
+  reducers another difficulties appear which cause me to use some unusual and
+  difficult workarounds (like accessing boardMap state prop from
+  score reducer to calculate new score).
+
+  */
+
 const game = (state = {
-    boardMap: new Array(9).fill(null),
+    boardMap: clearMap(),
     score: {
       x: 0,
       y: 0
@@ -37,7 +53,7 @@ const game = (state = {
         };
     } else if (action.type === 'RESTART_GAME') {
         return {
-          boardMap: new Array(9).fill(null),
+          boardMap: clearMap(),
           score: state.score
         };
     }
